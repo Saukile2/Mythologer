2ad6c35352912d9f351bcab1adc8264ea52966f8
Saukile2 <94383708+Saukile2@users.noreply.github.com>
2021-12-31 19:58:05 +0300

    Add files via upload

---
+++ Script/Script.js
@@ -0,0 +1,7 @@
+function openNav() {
+            document.getElementById("mySidebar3").style.width = "100%";
+        }
+
+        function closeNav() {
+            document.getElementById("mySidebar3").style.display = "none";
+        }
